f3304f2699a83dd0afb382742b898e97d83e78ac
m.Yeganeh <masoudyeganeh92@gmail.com>
2024-04-22 15:42:02 +0330

    14030203

--- .venv/Scripts/monthly_challenges/challenges/views.py
+++ .venv/Scripts/monthly_challenges/challenges/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseNotFound
+from django.http import HttpResponse,HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+monthly_challenges = {
+    "jan": "hi jan",
+    "feb": "hi feb"
+}
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("invalid month number")
+    redirected_month = months[month - 1]
+    redirected_path = reverse("month-challenge", args=[redirected_month])
+    return HttpResponseRedirect(redirected_path)
 
 def monthly_challenge(request, month):
-    x = ""
-    if month == "jan":
-        x = "jan!"
-    elif month == "feb":
-        x = "feb!"
-    else:
-        return HttpResponseNotFound("invalid month")
-    return HttpResponse(x)
-
-# Create your views here.
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("invalid")
